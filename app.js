5b8190a4b6d47d9715bf1008c5d97af054a2562a
Ruyi <ewq2895509@gmail.com>
2023-05-01 21:45:10 +0800

    thrid commit

--- app.js
+++ app.js
@@ -11,55 +11,55 @@ const openai = new OpenAIApi(configuration);
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.CHANNEL_SECRET,
-  };
+};
   
-  // create LINE SDK client
-  const client = new line.Client(config);
-  
-  // create Express app
-  // about Express itself: https://expressjs.com/
-  const app = express();
-  
-  // register a webhook handler with middleware
-  // about the middleware, please refer to doc
-  app.post('/callback', line.middleware(config), (req, res) => {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).end();
-      });
-  });
-  
-  // event handler
-  async function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      // ignore non-text-message event
-      return Promise.resolve(null);
-    }
-  
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: 'user',
-          content: event.message.text,
-        }],
-      max_tokens: 500
-    });
-    
-  
-    // create a echoing text message
-    const [choices] = data.choices;
-    const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
+// create LINE SDK client
+const client = new line.Client(config);
 
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
-  }  
-  
-  // listen on port
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`listening on ${port}`);
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  const { data } = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: 'user',
+        content: event.message.text,
+      }
+    ],
+    max_tokens: 500,
   });
+
+  // create a echoing text message
+  const [choices] = data.choices;
+  const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
